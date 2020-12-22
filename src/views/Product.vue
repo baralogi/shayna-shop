43f689f0a13f04d6be1e14d692aad9f2de13b4a6
Sebastianus Sembara <sembara9090@gmail.com>
2020-12-22 21:43:02 +0700

    Add @/components/RelatedItem.vue

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-12">
             <div class="breadcrumb-text product-more">
-              <a href="./home.html"><i class="fa fa-home"></i> Home</a>
+              <router-link to="/"><i class="fa fa-home"></i> Home</router-link>
               <span>Detail</span>
             </div>
           </div>
@@ -94,6 +94,7 @@
       </div>
     </section>
     <!-- Product Shop Section End -->
+    <RelatedItem />
     <Footer />
   </div>
 </template>
@@ -102,6 +103,7 @@
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import RelatedItem from "@/components/RelatedItem.vue";
 
 import Carousel from "vue-owl-carousel";
 
@@ -111,6 +113,7 @@ export default {
     Header,
     Footer,
     Carousel,
+    RelatedItem
   },
   data() {
     return {
